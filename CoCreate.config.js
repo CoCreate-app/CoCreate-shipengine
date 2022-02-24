86888c20df93886a87d760b431f7f53a670d0054
Frank <frank@cocreate.app>
2022-02-24 19:10:28 +0000

    fix: CoCreate.config replace CoCreate.app with *

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/shipengine/index.html",
                 "domains": [
-                    "cocreate.app",
+                    "*",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/shipengine",
